f63ece186e513b160c6706a3c6a23c64c9b40d2e
Yuliya Dubovik <feelgloom@gmail.com>
2023-11-22 23:04:49 +0300

    feat: rename file function

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import {rename as nodeRename, access} from 'node:fs/promises';
 const rename = async () => {
-    // Write your code here 
+    const wrongFilename = 'files/wrongFilename.txt';
+    const properFilename = 'files/properFilename.md';
+
+    try {
+        await access(properFilename);
+        throw new Error('FS operation failed');
+    } catch (error) {
+        if (error.code === 'ENOENT') {
+            await nodeRename(wrongFilename, properFilename);
+        } else {
+            throw error;
+        }
+    }
 };
 
 await rename();
