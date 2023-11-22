3b59648990d081fc222ae500445a6b4931367532
Yuliya Dubovik <feelgloom@gmail.com>
2023-11-22 23:54:13 +0300

    feat: cli tasks + markdown update

--- Readme.md
+++ Readme.md
@@ -2,3 +2,4 @@
 
 ### Documentation used:
 - [File System](https://nodejs.org/docs/latest/api/fs.html#fspromisesreaddirpath-options)
+- [Process / CLI / environment](https://nodejs.org/docs/latest/api/process.html#processargv)
--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,18 @@
+import {argv} from 'node:process';
 const parseArgs = () => {
-    // Write your code here 
+    // cli input:
+    // node args --propName value --prop2Name value2
+    // expected output:
+    // propName is value, prop2Name is value2
+    const argumentsSeparated = argv.slice(2);
+    let argumentsCombined = [];
+    for (let i = 0; i < argumentsSeparated.length; i = i + 2) {
+        if (argumentsSeparated[i + 1]) {
+            argumentsCombined.push(`${argumentsSeparated[i].slice(2)} is ${argumentsSeparated[i + 1]}`)
+        }
+    }
+    const result = argumentsCombined.join(', ')
+    console.log(result)
 };
 
 parseArgs();
