4b52060389ba27eab3e037dd6abbefed952de17b
Yuliya Dubovik <feelgloom@gmail.com>
2023-11-22 23:12:33 +0300

    feat: list filenames from directory function

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,13 @@
+import { readdir } from 'node:fs/promises';
 const list = async () => {
-    // Write your code here 
+    try {
+        const files = await readdir('files');
+        for (const file of files) {
+            console.log(file);
+        }
+    } catch (err) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await list();
