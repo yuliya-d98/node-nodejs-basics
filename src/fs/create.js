9a9d0ec9e582f50efb25818444faba174dd2aabf
Yuliya Dubovik <feelgloom@gmail.com>
2023-11-22 22:18:19 +0300

    feat: create file function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,18 @@
+import { access, appendFile, constants } from 'node:fs/promises';
 const create = async () => {
-    // Write your code here 
+    const filePath = "files/fresh.txt";
+    try {
+        await access(filePath);
+        // file exists
+        throw new Error('FS operation failed');
+    } catch(error) {
+        // no access to file
+        if (error.code === 'ENOENT') {
+            await appendFile(filePath, "I am fresh and young")
+        } else {
+            throw error;
+        }
+    }
 };
 
 await create();
