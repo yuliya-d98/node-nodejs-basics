a9092f20d1b8f2f557644904396b10ec873f9b8e
Yuliya Dubovik <feelgloom@gmail.com>
2023-11-26 18:03:32 +0300

    feat: read file using readable stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,13 @@
+import { createReadStream } from 'node:fs';
+import { stdout } from 'node:process';
+
 const read = async () => {
-    // Write your code here 
+    createReadStream('files/fileToRead.txt').pipe(stdout);
+
+    // const readableStream = createReadStream('files/fileToRead.txt', 'utf-8');
+    // readableStream.on('data', (chunk) => {
+    //     stdout.write(chunk);
+    // });
 };
 
 await read();
