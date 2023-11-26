296f6548b5b95cda9b0ce4a3af8bce81059cbbab
Yuliya Dubovik <feelgloom@gmail.com>
2023-11-26 18:14:57 +0300

    feat: fill file with stdin chunks using write stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,12 @@
+import { createWriteStream } from 'node:fs';
+import { stdin } from 'node:process';
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = createWriteStream('files/fileToWrite.txt');
+
+    stdin.on('data', (chunk) => {
+        writeStream.write(chunk);
+    })
 };
 
 await write();
