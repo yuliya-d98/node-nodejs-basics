e8fa6d213a9b5dc22265c511e6f39d38674b4cd8
Yuliya Dubovik <feelgloom@gmail.com>
2023-11-26 18:41:43 +0300

    feat: compress .txt file into .gz

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,18 @@
+import { createGzip } from 'node:zlib';
+import { pipeline } from 'node:stream';
+import {
+    createReadStream,
+    createWriteStream,
+} from 'node:fs';
+import { promisify } from 'node:util';
+
+// https://nodejs.org/api/zlib.html#zlib
+const pipe = promisify(pipeline);
 const compress = async () => {
-    // Write your code here 
+    const gzip = createGzip();
+    const source = createReadStream('files/fileToCompress.txt');
+    const destination = createWriteStream('files/archive.gz');
+    await pipe(source, gzip, destination);
 };
 
 await compress();
