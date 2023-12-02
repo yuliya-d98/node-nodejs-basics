211da2a449247fea4182cc03f1982a486a28117f
Yuliya Dubovik <feelgloom@gmail.com>
2023-12-02 18:14:13 +0300

    feat: create worker threads and sending data

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,8 +1,29 @@
 import os from 'node:os';
+import { Worker } from 'node:worker_threads';
 
 const performCalculations = async () => {
-    console.log(os.cpus().length)
-    // Write your code here
+    const cores = os.cpus().length;
+    const results = [];
+
+    for (let i = 0; i < cores; i++) {
+        const worker = new Worker('./worker.js', { workerData: i + 10 });
+
+        const logResults = () => {
+            if (results.length === cores) {
+                console.log(results);
+            }
+        }
+
+        worker.on('message', (result) => {
+            results[i] = { status: 'resolved', data: result };
+            logResults();
+        });
+
+        worker.on('error', (error) => {
+            results[i] = { status: 'error', data: null };
+            logResults();
+        });
+    }
 };
 
 await performCalculations();
