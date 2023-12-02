b71587c2e0d388d7013bdc4f3c542311522e36b3
Yuliya Dubovik <feelgloom@gmail.com>
2023-12-02 18:18:52 +0300

    feat: child processes task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,12 @@
+import { spawn } from 'node:child_process';
+import { stdin, stdout } from 'node:process';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', ['files/script.js', ...args]);
+
+    stdin.pipe(child.stdin);
+    child.stdout.pipe(stdout);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+await spawnChildProcess(['someArgument1', 'someArgument2']);
